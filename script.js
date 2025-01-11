051bc150565e12ebb30cc3ee20c5f0ee6d634585
Ronit <ronitkukadiya999@gmail.com>
2025-01-12 00:29:48 +0530

    Add files via upload
    this is a java-script mini project of random joke-generator using online public API

---
+++ script.js
@@ -0,0 +1,21 @@
+url = "https://official-joke-api.appspot.com/random_joke";
+let setup = document.querySelector(".setup");
+let punch = document.querySelector(".punch");
+let btn = document.querySelector(".random");
+setup.innerText = "";
+punch.innerText = "";
+async function getfact() {
+    btn.disabled = true;
+  let responce = await fetch(url);
+  let joke = await responce.json();
+  setup.innerText = joke.setup + "...";
+  setTimeout(() => {
+    punch.innerText = joke.punchline;
+    btn.disabled = false;
+  }, 3000);
+}
+btn.addEventListener("click", () => {
+  setup.innerText = "";
+  punch.innerText = "";
+  getfact();
+});
